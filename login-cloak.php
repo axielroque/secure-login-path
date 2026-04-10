54a7d841ca79a5a9593a12e88d836e86da9eaaea
Axiel Roque <axiel.roque@outlook.es>
2026-04-10 14:51:12 -0400

    chore: rename display to RoqWarden Login Path

--- login-cloak.php
+++ login-cloak.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Login Cloak – Hide & Protect Your Login URL
+Plugin Name: RoqWarden Login Path
 Plugin URI: https://github.com/axielroque/login-cloak
-Description: Login Cloak helps you secure your website by hiding the default login URLs (/wp-admin and /wp-login.php) and replacing them with a custom path only you know. This simple change can dramatically reduce brute-force attacks and unauthorized access attempts.
+Description: RoqWarden Login Path helps you secure your website by hiding the default login URLs (/wp-admin and /wp-login.php) and replacing them with a custom path only you know. This simple change can dramatically reduce brute-force attacks and unauthorized access attempts.
 Version: 1.1.1
 Author: Axiel Roque
 Author URI: https://github.com/axielroque
